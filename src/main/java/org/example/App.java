0118fb0a21212e160cebc52d613b102906e22491
JRobes <tarres.javier@gmail.com>
2026-03-16 13:06:50 +0100

    Todo vuelve a fallar...

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -24,24 +24,28 @@ import java.util.List;
 public class App 
 {
     private static int lines2Skip = 1;
-    private static int minibatchSize = 6; //Number of examples in each minibatch
+    private static int minibatchSize = 4; //Number of examples in each minibatch
     private static int windowSize = 7;
     private static int labelColumn = 0;
     private static double percentageOfTest = 0.2; //Entero de 1 a 100
     public static void main( String[] args ) throws IOException, InterruptedException {
         System.out.println( "Hello World!" );
-        List<String> lines = Files.readAllLines(Paths.get("test.csv"));
+        List<String> lines = Files.readAllLines(Paths.get("test-2.csv"));
         System.out.println("En archivo csv número total de líneas: " + lines.size());
         int totalSamples = lines.size()-lines2Skip;
         int numFeatures = lines.get(lines2Skip).split(",").length;
         System.out.println("Num lineas con datos: " + totalSamples + "\tNúmero features: " + numFeatures);
-        double[][] matrix = new double[totalSamples][numFeatures];
+        //double[][] matrix = new double[totalSamples][numFeatures];
+        String[][] matrix = new String[totalSamples][numFeatures];
 
         for (int i = lines2Skip; i < totalSamples; i++) { //saltar las lineas indicadas
 
             String[] values = lines.get(i).split(",");
             for (int j = 0; j < numFeatures; j++) {
-                matrix[i - lines2Skip][j] = Double.parseDouble(values[j]);
+
+                matrix[i - lines2Skip][j] = (values[j]);
+
+                //matrix[i - lines2Skip][j] = Double.parseDouble(values[j]);
                 //System.out.print(matrix[i][j] + "\t");
             }
             //System.out.println();
@@ -56,20 +60,25 @@ public class App
         System.out.println("Secuencias de test: " + testSequences);
         int trainSequences = numSequences - testSequences;
 
-        INDArray testLabels = Nd4j.create(testSequences, 1, windowSize);
+        //INDArray testLabels = Nd4j.create(minibatchSize, 1, windowSize);
+        String[][][] testLabels = new String[minibatchSize][1][windowSize];
         for (int i = 0; i < testSequences; i++) {
             for (int t = 0; t < windowSize; t++) {
-
-               // for (int f = 0; f < 4; f++) {
-               //     input.putScalar(new int[]{i, f, t}, data[i+t][f]);
-               // }
-
-                testLabels.putScalar(new int[]{i, 0, t}, matrix[i+t][0]);
+                testLabels[i][0][t] = matrix[i+t][0];
             }
         }
-        //System.out.println("Test labels shape: " + Arrays.toString(testLabels.shape()));
-        //System.out.println(testLabels);
+        //System.out.println("Test labels shape: " + Arrays.toString(testLabels));
+        //System.out.println(testLabels.);
+        for (int i = 0; i < testLabels.length; i++) {
+            for (int j = 0; j < testLabels[i].length; j++) {
+                for (int k = 0; k < testLabels[i][j].length; k++) {
+                    System.out.println("[" + i + "][" + j + "][" + k + "] = " + testLabels[i][j][k]);
+                }
+            }
+        }
 
+
+        /*
         INDArray trainLabels = Nd4j.create(numSequences - testSequences, 1, windowSize);
         for (int i = 0; i < trainSequences; i++) {
             for (int t = 0; t < windowSize; t++) {
@@ -80,9 +89,17 @@ public class App
         //System.out.println("Train labels shape: " + Arrays.toString(trainLabels.shape()));
         //System.out.println(trainLabels);
 
-
-
-
+        INDArray testData = Nd4j.create(testSequences,numFeatures, windowSize);
+        for (int i = 0; i < testSequences; i++) {
+            for (int t = 0; t < windowSize; t++) {
+                for (int f = 0; f < 4; f++) {
+                    testData.putScalar(new int[]{i, f, t}, matrix[i+t][f]);
+                 }
+            }
+        }
+        //System.out.println("Test data shape: " + Arrays.toString(testData.shape()));
+        //System.out.println(testData);
+*/
 
 
 
