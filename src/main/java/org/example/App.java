ac1e233df6fab4e7d3fb6377fc55c81c09ac582e
JRobes <tarres.javier@gmail.com>
2026-03-16 09:48:06 +0100

    Subir test.csv. Ya testá el NDArray de labels para test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,33 +24,52 @@ import java.util.List;
 public class App 
 {
     private static int lines2Skip = 1;
-    private static int minibatchSize = 10; //Number of examples in each minibatch
-    private static int windowSize = 60;
+    private static int minibatchSize = 6; //Number of examples in each minibatch
+    private static int windowSize = 7;
     private static int labelColumn = 0;
     private static double percentageOfTest = 0.2; //Entero de 1 a 100
     public static void main( String[] args ) throws IOException, InterruptedException {
         System.out.println( "Hello World!" );
-        List<String> lines = Files.readAllLines(Paths.get("Gold Price.csv"));
+        List<String> lines = Files.readAllLines(Paths.get("test.csv"));
         System.out.println("En archivo csv número total de líneas: " + lines.size());
         int totalSamples = lines.size()-lines2Skip;
-        int numFeatures = lines.get(0).split(",").length;
+        int numFeatures = lines.get(lines2Skip).split(",").length;
         System.out.println("Num lineas con datos: " + totalSamples + "\tNúmero features: " + numFeatures);
         double[][] matrix = new double[totalSamples][numFeatures];
 
         for (int i = lines2Skip; i < totalSamples; i++) { //saltar las lineas indicadas
+
             String[] values = lines.get(i).split(",");
             for (int j = 0; j < numFeatures; j++) {
-                matrix[i][j] = Double.parseDouble(values[j]);
+                matrix[i - lines2Skip][j] = Double.parseDouble(values[j]);
+                //System.out.print(matrix[i][j] + "\t");
             }
+            //System.out.println();
         }
         if(windowSize + 1 > totalSamples){
-            System.out.println("El númeor total de muestras +1 es menor que el tamaño de la ventana, no sale ni una secuencia... se cierra el programa");
+            System.out.println("El número total de muestras + 1 es menor que el tamaño de la ventana, no sale ni una secuencia... se cierra el programa");
             return;
         }
         int numSequences = totalSamples - windowSize + 1 - 1; //Ojo hay que quitar la primera linea que solo label
         System.out.println("Número de secuencias: " + numSequences);
         int testSequences = (int)(numSequences*percentageOfTest);
         System.out.println("Secuencias de test: " + testSequences);
+        double[][][] labelTestArray = new double[minibatchSize][1][1];//
+
+        INDArray labels = Nd4j.create(minibatchSize, 1, windowSize);
+        for (int i = 0; i < minibatchSize; i++) {
+            for (int t = 0; t < windowSize; t++) {
+
+               // for (int f = 0; f < 4; f++) {
+               //     input.putScalar(new int[]{i, f, t}, data[i+t][f]);
+               // }
+
+                labels.putScalar(new int[]{i, 0, t}, matrix[i+t][0]);
+            }
+        }
+        System.out.println(Arrays.toString(labels.shape()));
+
+        System.out.println(labels);
 
 
         //INDArray data = Nd4j.create(matrix);
